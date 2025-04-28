35f5bbcde4c8edf0627d53847ebeebbf8a16dbac
Tekalig <tekahazi06@gmail.com>
2025-04-28 10:38:43 +0300

    Refactor User model and UserManager for improved clarity and structure

--- launge/core/models.py
+++ launge/core/models.py
@@ -1,3 +1,62 @@
+"""
+Database models for the Launge application.
+This module contains the database models for the Launge application. It includes
+the User model, which is a custom user model that extends the AbstractBaseUser and PermissionsMixin classes from Django's auth module.
+It also includes the UserManager class, which is responsible for creating user and superuser instances.
+The User model includes fields for email, name, and other user-related information.
+"""
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Custom manager for User model
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a user with an email and password.
+        """
+        if not email:
+            raise ValueError("Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User model in a system that uses email instead of username.
+    """
+
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
