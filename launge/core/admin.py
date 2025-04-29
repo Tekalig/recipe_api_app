e39b9f470c2d3d316cdadad1480c261013d0ba78
Tekalig <tekahazi06@gmail.com>
2025-04-29 17:50:15 +0300

    Enhance admin interface for User model with custom UserAdmin class

--- launge/core/admin.py
+++ launge/core/admin.py
@@ -1,3 +1,69 @@
-from django.contrib import admin
+"""
+Settings for the admin interface of the Launge application.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin for the user model.
+    """
+
+    ordering = ["id"]
+    list_display = [
+        "email",
+        "name",
+    ]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    readonly_fields = ["last_login"]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
